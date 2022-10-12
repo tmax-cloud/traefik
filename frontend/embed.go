7b4a380a1d520ab743f1fe284a91099c3b4cdf5d
jinsoo_youn <jinsoo_youn@tmax.co.kr>
2022-10-12 17:25:36 +0900

    update embed.go to see static folder

--- frontend/embed.go
+++ frontend/embed.go
@@ -5,8 +5,8 @@ import (
 	"io/fs"
 )
 
-//go:embed public/dist
+//go:embed static
 var assets embed.FS
 
 // FS contains the web UI assets.
-var FS, _ = fs.Sub(assets, "public/dist")
+var FS, _ = fs.Sub(assets, "static")
